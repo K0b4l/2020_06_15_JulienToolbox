94cab009861e8cd35f66033bdbfff10feb7d7d67
Julien Coppenolle <58251569+K0b4l@users.noreply.github.com>
2020-06-15 14:56:02 +0200

    add Runtime, runtime>changecoloronclick and Editor, Editor<changecoloronclickeditor

---
+++ Runtime/ChangeColorOnClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnClick : MonoBehaviour
+{
+    public GameObject _colorChangeTarget;
+    public Color _startingColor;
+
+    Color _previousColor;
+
+    private void Start()
+    {
+        _colorChangeTarget.GetComponent<Renderer>().material.color = _startingColor;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _previousColor = _colorChangeTarget.GetComponent<Renderer>().material.color;
+            _colorChangeTarget.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _colorChangeTarget.GetComponent<Renderer>().material.color = _previousColor;
+        }
+    }
+}
